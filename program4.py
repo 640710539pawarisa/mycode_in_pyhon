c03bd9ce8a998008c1a807c4e2e675d960ca4957
pawarisa <rattanathiantho_p@silpakorn.edu>
2025-05-20 14:47:53 +0700

    เพิ่มโค้ด

---
+++ program4.py
@@ -0,0 +1,22 @@
+#ตัวดำเนินการทางคณิตศาสตร์
+
+number1,number2=5,2
+print("ตัวเลขที่ 1 = ",number1)
+print("ตัวเลขที่ 2 = ",number2)
+
+#คำนวณ
+print("=========================")
+print("ผลบวก = ",number1+number2)
+print("=========================")
+print("ผลลบ = ",number1-number2)
+print("=========================")
+print("ผลคูณ = ",number1*number2)
+print("=========================")
+print("ยกกำลัง = ",number1**number2)
+print("=========================")
+print("ผลหาร = ",number1/number2)
+print("=========================")
+print("ผลหารไม่เอาจุดทศนิยม = ",number1//number2)
+print("=========================")
+print("ผลเศษจากการหาร = ",number1%number2)
+
